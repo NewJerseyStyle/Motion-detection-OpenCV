3ce5f2541113029a7fd72db6486ca5737c344196
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2020-03-31 12:19:07 +0800

    Update MotionDetectorContours.py to OpenCV 3 above

--- MotionDetectorContours.py
+++ MotionDetectorContours.py
@@ -1,4 +1,5 @@
-import cv2.cv as cv
+import cv2 as cv
+import numpy as np
 from datetime import datetime
 import time
 
@@ -14,38 +15,37 @@ class MotionDetectorAdaptative():
         self.show = showWindows #Either or not show the 2 windows
         self.frame = None
     
-        self.capture=cv.CaptureFromCAM(0)
-        self.frame = cv.QueryFrame(self.capture) #Take a frame to init recorder
+        self.capture=cv.VideoCapture(0)
+        self.frame = self.capture.read()[1] #Take a frame to init recorder
         if doRecord:
             self.initRecorder()
         
-        self.gray_frame = cv.CreateImage(cv.GetSize(self.frame), cv.IPL_DEPTH_8U, 1)
-        self.average_frame = cv.CreateImage(cv.GetSize(self.frame), cv.IPL_DEPTH_32F, 3)
         self.absdiff_frame = None
         self.previous_frame = None
         
-        self.surface = self.frame.width * self.frame.height
+        self.surface = self.frame.shape[0] * self.frame.shape[1]
         self.currentsurface = 0
         self.currentcontours = None
         self.threshold = threshold
         self.isRecording = False
         self.trigger_time = 0 #Hold timestamp of the last detection
+        self.es = cv.getStructuringElement(cv.MORPH_ELLIPSE, (9,4))
         
         if showWindows:
-            cv.NamedWindow("Image")
-            cv.CreateTrackbar("Detection treshold: ", "Image", self.threshold, 100, self.onChange)
+            cv.namedWindow("Image")
+            cv.createTrackbar("Detection treshold: ", "Image", self.threshold, 100, self.onChange)
         
     def initRecorder(self): #Create the recorder
-        codec = cv.CV_FOURCC('M', 'J', 'P', 'G')
-        self.writer=cv.CreateVideoWriter(datetime.now().strftime("%b-%d_%H_%M_%S")+".wmv", codec, 5, cv.GetSize(self.frame), 1)
+        codec = cv.VideoWriter_fourcc('M', 'J', 'P', 'G')
+        self.writer=cv.VideoWriter(datetime.now().strftime("%b-%d_%H_%M_%S")+".wmv", codec, 5, self.frame.shape[1::-1], 1)
         #FPS set to 5 because it seems to be the fps of my cam but should be ajusted to your needs
-        self.font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 2, 8) #Creates a font
+        self.font = cv.FONT_HERSHEY_SIMPLEX #Creates a font
 
     def run(self):
         started = time.time()
         while True:
             
-            currentframe = cv.QueryFrame(self.capture)
+            currentframe = self.capture.read()[1]
             instant = time.time() #Get timestamp o the frame
             
             self.processImage(currentframe) #Process the image
@@ -54,57 +54,54 @@ class MotionDetectorAdaptative():
                 if self.somethingHasMoved():
                     self.trigger_time = instant #Update the trigger_time
                     if instant > started +10:#Wait 5 second after the webcam start for luminosity adjusting etc..
-                        print "Something is moving !"
+                        print("Something is moving !")
                         if self.doRecord: #set isRecording=True only if we record a video
                             self.isRecording = True
-                cv.DrawContours (currentframe, self.currentcontours, (0, 0, 255), (0, 255, 0), 1, 2, cv.CV_FILLED)
+                currentframe = cv.drawContours(currentframe, self.currentcontours, -1, (0, 255, 0), cv.FILLED)
             else:
                 if instant >= self.trigger_time +10: #Record during 10 seconds
-                    print "Stop recording"
+                    print("Stop recording")
                     self.isRecording = False
                 else:
-                    cv.PutText(currentframe,datetime.now().strftime("%b %d, %H:%M:%S"), (25,30),self.font, 0) #Put date on the frame
-                    cv.WriteFrame(self.writer, currentframe) #Write the frame
+                    cv.putText(currentframe,datetime.now().strftime("%b %d, %H:%M:%S"), (25,30),self.font, 1, (255, 0, 0), 2, cv.LINE_AA) #Put date on the frame
+                    self.writer.write(currentframe) #Write the frame
             
             if self.show:
-                cv.ShowImage("Image", currentframe)
+                cv.imshow("Image", currentframe)
                 
-            c=cv.WaitKey(1) % 0x100
+            c=cv.waitKey(1) % 0x100
             if c==27 or c == 10: #Break if user enters 'Esc'.
                 break            
     
     def processImage(self, curframe):
-            cv.Smooth(curframe, curframe) #Remove false positives
+            curframe = cv.GaussianBlur(curframe, (21,21), 0) #Remove false positives
             
-            if not self.absdiff_frame: #For the first time put values in difference, temp and moving_average
-                self.absdiff_frame = cv.CloneImage(curframe)
-                self.previous_frame = cv.CloneImage(curframe)
-                cv.Convert(curframe, self.average_frame) #Should convert because after runningavg take 32F pictures
+            if self.absdiff_frame is None: #For the first time put values in difference, temp and moving_average
+                self.absdiff_frame = curframe.copy()
+                self.previous_frame = curframe.copy()
+                self.average_frame = np.float32(curframe) #Should convert because after runningavg take 32F pictures
             else:
-                cv.RunningAvg(curframe, self.average_frame, 0.05) #Compute the average
+                cv.accumulateWeighted(curframe, self.average_frame, 0.05) #Compute the average
             
-            cv.Convert(self.average_frame, self.previous_frame) #Convert back to 8U frame
+            self.previous_frame = self.average_frame.astype(np.uint8) #Convert back to 8U frame
             
-            cv.AbsDiff(curframe, self.previous_frame, self.absdiff_frame) # moving_average - curframe
+            self.absdiff_frame = cv.absdiff(curframe, self.previous_frame) # moving_average - curframe
             
-            cv.CvtColor(self.absdiff_frame, self.gray_frame, cv.CV_RGB2GRAY) #Convert to gray otherwise can't do threshold
-            cv.Threshold(self.gray_frame, self.gray_frame, 50, 255, cv.CV_THRESH_BINARY)
+            self.gray_frame = cv.cvtColor(self.absdiff_frame, cv.COLOR_BGR2GRAY) #Convert to gray otherwise can't do threshold
+            self.gray_frame = cv.threshold(self.gray_frame, 5, 255, cv.THRESH_BINARY)[1]
 
-            cv.Dilate(self.gray_frame, self.gray_frame, None, 15) #to get object blobs
-            cv.Erode(self.gray_frame, self.gray_frame, None, 10)
+            self.gray_frame = cv.dilate(self.gray_frame, self.es) #to get object blobs
+            # cv.Erode(self.gray_frame, self.gray_frame, None, 10)
 
             
     def somethingHasMoved(self):
         
         # Find contours
-        storage = cv.CreateMemStorage(0)
-        contours = cv.FindContours(self.gray_frame, storage, cv.CV_RETR_EXTERNAL, cv.CV_CHAIN_APPROX_SIMPLE)
+        contours = cv.findContours(self.gray_frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
 
         self.currentcontours = contours #Save contours
         
-        while contours: #For all contours compute the area
-            self.currentsurface += cv.ContourArea(contours)
-            contours = contours.h_next()
+        self.currentsurface = sum([cv.contourArea(c) for c in contours]) #For all contours compute the area
         
         avg = (self.currentsurface*100)/self.surface #Calculate the average of contour area on the total size
         self.currentsurface = 0 #Put back the current surface to 0
@@ -116,5 +113,5 @@ class MotionDetectorAdaptative():
 
         
 if __name__=="__main__":
-    detect = MotionDetectorAdaptative(doRecord=True)
+    detect = MotionDetectorAdaptative(threshold=5, doRecord=True)
     detect.run()
