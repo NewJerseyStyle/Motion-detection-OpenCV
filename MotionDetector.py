b5222b113b76b68b82ba311b6f7826ab6a9b4b2c
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2020-03-31 12:40:13 +0800

    Update to Python3 and OpenCV3 above

--- MotionDetector.py
+++ MotionDetector.py
@@ -1,4 +1,5 @@
-import cv2.cv as cv
+import cv2 as cv
+import numpy as np
 from datetime import datetime
 import time
 
@@ -14,41 +15,42 @@ class MotionDetectorInstantaneous():
         self.show = showWindows #Either or not show the 2 windows
         self.frame = None
     
-        self.capture=cv.CaptureFromCAM(0)
-        self.frame = cv.QueryFrame(self.capture) #Take a frame to init recorder
+        self.capture=cv.VideoCapture(0)
+        self.frame = self.capture.read()[1] #Take a frame to init recorder
         if doRecord:
             self.initRecorder()
         
-        self.frame1gray = cv.CreateMat(self.frame.height, self.frame.width, cv.CV_8U) #Gray frame at t-1
-        cv.CvtColor(self.frame, self.frame1gray, cv.CV_RGB2GRAY)
+        self.frame1gray = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY).astype(np.uint8) #Convert to gray otherwise can't do threshold
         
         #Will hold the thresholded result
-        self.res = cv.CreateMat(self.frame.height, self.frame.width, cv.CV_8U)
+        self.res = np.zeros(self.frame.shape[1::-1]).astype(np.uint8)
         
-        self.frame2gray = cv.CreateMat(self.frame.height, self.frame.width, cv.CV_8U) #Gray frame at t
+        self.frame2gray = np.zeros(self.frame.shape[1::-1]).astype(np.uint8) #Gray frame at t
         
-        self.width = self.frame.width
-        self.height = self.frame.height
+        self.width = self.frame.shape[0]
+        self.height = self.frame.shape[1]
         self.nb_pixels = self.width * self.height
         self.threshold = threshold
         self.isRecording = False
         self.trigger_time = 0 #Hold timestamp of the last detection
+
+        self.kernel = cv.getStructuringElement(cv.MORPH_RECT,(5,5))
         
         if showWindows:
-            cv.NamedWindow("Image")
-            cv.CreateTrackbar("Detection treshold: ", "Image", self.threshold, 100, self.onChange)
+            cv.namedWindow("Image")
+            cv.createTrackbar("Detection treshold: ", "Image", self.threshold, 100, self.onChange)
         
     def initRecorder(self): #Create the recorder
-        codec = cv.CV_FOURCC('M', 'J', 'P', 'G') #('W', 'M', 'V', '2')
-        self.writer=cv.CreateVideoWriter(datetime.now().strftime("%b-%d_%H_%M_%S")+".wmv", codec, 5, cv.GetSize(self.frame), 1)
+        codec = cv.VideoWriter_fourcc('M', 'J', 'P', 'G') #('W', 'M', 'V', '2')
+        self.writer=cv.VideoWriter(datetime.now().strftime("%b-%d_%H_%M_%S")+".wmv", codec, 5, self.frame.shape[1::-1], 1)
         #FPS set to 5 because it seems to be the fps of my cam but should be ajusted to your needs
-        self.font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 2, 8) #Creates a font
+        self.font = cv.FONT_HERSHEY_SIMPLEX #Creates a font
 
     def run(self):
         started = time.time()
         while True:
             
-            curframe = cv.QueryFrame(self.capture)
+            curframe = self.capture.read()[1]
             instant = time.time() #Get timestamp o the frame
             
             self.processImage(curframe) #Process the image
@@ -57,42 +59,42 @@ class MotionDetectorInstantaneous():
                 if self.somethingHasMoved():
                     self.trigger_time = instant #Update the trigger_time
                     if instant > started +5:#Wait 5 second after the webcam start for luminosity adjusting etc..
-                        print datetime.now().strftime("%b %d, %H:%M:%S"), "Something is moving !"
+                        print(datetime.now().strftime("%b %d, %H:%M:%S"), "Something is moving !")
                         if self.doRecord: #set isRecording=True only if we record a video
                             self.isRecording = True
             else:
                 if instant >= self.trigger_time +10: #Record during 10 seconds
-                    print datetime.now().strftime("%b %d, %H:%M:%S"), "Stop recording"
+                    print(datetime.now().strftime("%b %d, %H:%M:%S"), "Stop recording")
                     self.isRecording = False
                 else:
-                    cv.PutText(curframe,datetime.now().strftime("%b %d, %H:%M:%S"), (25,30),self.font, 0) #Put date on the frame
-                    cv.WriteFrame(self.writer, curframe) #Write the frame
+                    cv.putText(curframe,datetime.now().strftime("%b %d, %H:%M:%S"), (25,30),self.font, 1, (255, 0, 0), 2, cv.LINE_AA) #Put date on the frame
+                    self.writer.write(curframe) #Write the frame
             
             if self.show:
-                cv.ShowImage("Image", curframe)
-                cv.ShowImage("Res", self.res)
+                cv.imshow("Image", curframe)
+                cv.imshow("Res", self.res)
                 
-            cv.Copy(self.frame2gray, self.frame1gray)
-            c=cv.WaitKey(1) % 0x100
+            self.frame2gray = self.frame1gray.copy()
+            c=cv.waitKey(1) % 0x100
             if c==27 or c == 10: #Break if user enters 'Esc'.
                 break            
     
     def processImage(self, frame):
-        cv.CvtColor(frame, self.frame2gray, cv.CV_RGB2GRAY)
+        self.frame2gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #Convert to gray otherwise can't do threshold
         
         #Absdiff to get the difference between to the frames
-        cv.AbsDiff(self.frame1gray, self.frame2gray, self.res)
+        self.res = cv.absdiff(self.frame1gray, self.frame2gray)
         
         #Remove the noise and do the threshold
-        cv.Smooth(self.res, self.res, cv.CV_BLUR, 5,5)
-        cv.MorphologyEx(self.res, self.res, None, None, cv.CV_MOP_OPEN)
-        cv.MorphologyEx(self.res, self.res, None, None, cv.CV_MOP_CLOSE)
-        cv.Threshold(self.res, self.res, 10, 255, cv.CV_THRESH_BINARY_INV)
+        self.res = cv.GaussianBlur(self.res, (21,21), 0) #Remove false positives
+        self.res = cv.morphologyEx(self.res, cv.MORPH_OPEN, self.kernel)
+        self.res = cv.morphologyEx(self.res, cv.MORPH_CLOSE, self.kernel)
+        self.res = cv.threshold(self.res, 10, 255, cv.THRESH_BINARY_INV)[1]
 
     def somethingHasMoved(self):
         nb=0 #Will hold the number of black pixels
         min_threshold = (self.nb_pixels/100) * self.threshold #Number of pixels for current threshold
-        nb = self.nb_pixels - cv.CountNonZero(self.res)
+        nb = self.nb_pixels - cv.countNonZero(self.res)
         if (nb) > min_threshold:
            return True
         else:
